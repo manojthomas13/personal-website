117d85e4821739d34db0312e0cef431ee5acbb4f
Manoj Thomas <manoj.pk.thomas@gmail.com>
2021-04-16 12:55:42 +0000

    pull data from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const space = process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID
+const accessToken = process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+
+const client = require('contentful').createClient({
+  space: space,
+  accessToken: accessToken,
+})
+
+async function fetchData() {
+  const entries = await client.getEntries()
+
+  if (entries.items) return entries.items
+  console.log(`Error getting Entries for ${contentType.name}.`)
+}
+
+export default function Home(props) {
+  const { desc } = props
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +29,18 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to my site
         </h1>
-        </main>
+        <p>{desc}</p>
+      </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetchData()
+
+  return {
+    props: {
+      desc:response[0].fields.aboutMe,
+    },
+  }
 }
